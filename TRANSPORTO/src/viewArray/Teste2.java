f0f800232374636a28ae0d50c08acd0fa3517690
Letícia Vieira <91754673+1993LETICIA@users.noreply.github.com>
2023-03-27 22:42:49 -0300

    Update Teste2.java

--- TRANSPORTO/src/viewArray/Teste2.java
+++ TRANSPORTO/src/viewArray/Teste2.java
@@ -8,11 +8,11 @@ import array.Caminhoneiro2;
 
 public class Teste2 {
     public static void main(String[] args) {
-    Carreta2 car = new Carreta2("Carreta ct-7000");
-    Caminhoneiro2 cami = new Caminhoneiro2("João Alves", "12345678901234", "98765432109", 3000.0);
+    Carreta2 carreta = new Carreta2("Carreta ct-7000");
+    Caminhoneiro2 caminhoneiro = new Caminhoneiro2("João Alves", "12345678901234", "98765432109", 3000.0);
     
     Viagem2 viagem = new Viagem2("São Paulo SP", 4.5, 350.0);
-    Cliente2 cliente = new Cliente2("Pedro", cami, viagem, car);
+    Cliente2 cliente = new Cliente2("Pedro", caminhoneiro, viagem, carreta);
 
     Viagem2 viagem1 = new Viagem2("Rio de Janeiro", 3.0, 600.0);
     cliente.adicionarViagem(viagem1);
